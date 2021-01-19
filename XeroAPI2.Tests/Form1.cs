fba45f3a989855112e38dbe6363f965e99eadd89
AGenius <github@enigmaware.co.uk>
2021-01-19 23:44:09 +0000

    Add files via upload

--- XeroAPI2.Tests/Form1.cs
+++ XeroAPI2.Tests/Form1.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,18 +13,15 @@ using System.Windows.Forms;
 namespace XeroAPI2Tests
 {
     public partial class Form1 : Form
-    {        
+    {
         string XeroClientID = "Your Client ID";
-        Uri XeroCallbackUri = new Uri("http://localhost:8888/callback");        
+        Uri XeroCallbackUri = new Uri("http://localhost:8888/callback");
         string XeroState = "123456";
 
         public static string ApplicationPath = System.IO.Directory.GetParent(System.Reflection.Assembly.GetEntryAssembly().Location).FullName;
-        XeroAuth2API.Model.XeroOAuthToken accessToken = new XeroAuth2API.Model.XeroOAuthToken();
 
         XeroAuth2API.Model.XeroConfiguration XeroConfig = null;
         XeroAuth2API.API xeroAPI = null;
-
-        static FileSystemWatcher LogWatcher = null; // Used to watch a file system folder.
 
         public Form1()
         {
@@ -32,118 +29,121 @@ namespace XeroAPI2Tests
         }
         private void Form1_Load(object sender, EventArgs e)
         {
+            // We can ether save/restore the entire config or just the AccessToken element
             string tokendata = ReadTextFile("tokendata.txt");
             if (!string.IsNullOrEmpty(tokendata))
             {
-                accessToken = DeSerializeObject<XeroAuth2API.Model.XeroOAuthToken>(tokendata);
+                XeroConfig = DeSerializeObject<XeroAuth2API.Model.XeroConfiguration>(tokendata);
             }
             else
             {
-                accessToken = new XeroAuth2API.Model.XeroOAuthToken();
+                // Setup New Config
+                XeroConfig = new XeroAuth2API.Model.XeroConfiguration
+                {
+                    ClientID = XeroClientID,
+                    CallbackUri = XeroCallbackUri,
+                    // Add them this way or see below
+                    //Scopes = new List<XeroAuth2API.XeroScope> { XeroAuth2API.XeroScope.openid, XeroAuth2API.XeroScope.profile, XeroAuth2API.XeroScope.email, XeroAuth2API.XeroScope.accounting_contacts },
+                    State = XeroState, // Optional - Not needed for a desktop app
+                    codeVerifier = null // Code verifier will be generated if empty
+                };
+
+                XeroConfig.AddScope(XeroAuth2API.XeroScope.all);
+                // Or add idividualy
+                //XeroConfig.AddScope(XeroAuth2API.XeroScope.files);
+                //XeroConfig.AddScope(XeroAuth2API.XeroScope.accounting_transactions);
+                //XeroConfig.AddScope(XeroAuth2API.XeroScope.accounting_reports_read);
+                //XeroConfig.AddScope(XeroAuth2API.XeroScope.accounting_journals_read);
+                //XeroConfig.AddScope(XeroAuth2API.XeroScope.accounting_settings_read);
+                //XeroConfig.AddScope(XeroAuth2API.XeroScope.accounting_contacts);
+                //XeroConfig.AddScope(XeroAuth2API.XeroScope.assets);               
             }
 
-            XeroConfig = new XeroAuth2API.Model.XeroConfiguration
-            {
-                ClientID = XeroClientID,
-                CallbackUri = XeroCallbackUri,
-                // Add them this way or see below
-                //Scopes = new List<XeroAuth2API.XeroScope> { XeroAuth2API.XeroScope.openid, XeroAuth2API.XeroScope.profile, XeroAuth2API.XeroScope.email, XeroAuth2API.XeroScope.accounting_contacts },
-                State = XeroState, // Optional - Not needed for a desktop app
-                codeVerifier = null // Code verifier will be generated if empty
-            };
-
-            XeroConfig.AddScope(XeroAuth2API.XeroScope.all);
-            // Or add idividualy
-            //XeroConfig.AddScope(XeroAuth2API.XeroScope.files);
-            //XeroConfig.AddScope(XeroAuth2API.XeroScope.accounting_transactions);
-            //XeroConfig.AddScope(XeroAuth2API.XeroScope.accounting_reports_read);
-            //XeroConfig.AddScope(XeroAuth2API.XeroScope.accounting_journals_read);
-            //XeroConfig.AddScope(XeroAuth2API.XeroScope.accounting_settings_read);
-            //XeroConfig.AddScope(XeroAuth2API.XeroScope.accounting_contacts);
-            //XeroConfig.AddScope(XeroAuth2API.XeroScope.assets);
-
-            xeroAPI = new XeroAuth2API.API(XeroConfig, accessToken);
+            // Restore saved config
+            xeroAPI = new XeroAuth2API.API(XeroConfig);
+            //xeroAPI.XeroConfig = XeroConfig;
+            // Save this
+            tokendata = SerializeObject(XeroConfig);
+            WriteTextFile("tokendata.txt", tokendata);
             xeroAPI.StatusUpdates += StatusUpdates; // Bind to the status update event 
 
         }
         private void simpleButton1_Click(object sender, EventArgs e)
         {
-
-
             xeroAPI.InitializeAPI(); // Init the API and pass in the old token - this will be refreshed if required
 
             // Write the AccessToken to storage
-            string tokendata = SerializeObject(xeroAPI.XeroToken);
+            string tokendata = SerializeObject(XeroConfig);
             WriteTextFile("tokendata.txt", tokendata);
 
             // Find the Demo Company TenantID
             XeroAuth2API.Model.Tenant Tenant = xeroAPI.Tenants.Find(x => x.TenantName.ToLower() == "demo company (uk)");
             //  XeroAuth2API.Model.Tenant Tenant = xeroAPI.Tenants[1];
-            xeroAPI.TenantID = Tenant.TenantId.ToString(); // Ensure its selected
+            xeroAPI.SelectedTenant = Tenant; // Ensure its selected
 
             // var assettypes = xeroAPI.AssetTypes();
 
-            var contacts = xeroAPI.Contacts();
+            //var contacts = xeroAPI.Contacts();
 
-            try
-            {
-                var assets = xeroAPI.Assets(Xero.NetStandard.OAuth2.Model.Asset.AssetStatusQueryParam.REGISTERED);
-            }
-            catch (Exception ex)
-            {
-                // Deal with the error
-                int stop = 0;
-            }
-
-
-
-            var POrders = xeroAPI.PurchaseOrders();
-            if (POrders != null && POrders.Count > 0)
-            {
-                var singlePO = xeroAPI.PurchaseOrder(POrders[0].PurchaseOrderID.Value);
-            }
-
-            var users = xeroAPI.Users();
+            //try
+            //{
+            //    var assets = xeroAPI.Assets(Xero.NetStandard.OAuth2.Model.Asset.AssetStatusQueryParam.REGISTERED);
+            //}
+            //catch (Exception ex)
+            //{
+            //    // Deal with the error
+            //    int stop = 0;
+            //}
 
 
-            var trackingcats = xeroAPI.TrackingCategories();
-            if (trackingcats != null && trackingcats.Count > 0)
-            {
-                var singlecat = xeroAPI.TrackingCategory(trackingcats[0].TrackingCategoryID.Value);
-            }
+
+            //var POrders = xeroAPI.PurchaseOrders();
+            //if (POrders != null && POrders.Count > 0)
+            //{
+            //     var singlePO = xeroAPI.PurchaseOrder(POrders[0].PurchaseOrderID.Value);
+            //}
+
+            //var users = xeroAPI.Users();
 
 
-            var banktrans = xeroAPI.BankTransactions();
-            var singleptran = xeroAPI.BankTransaction(banktrans[3].BankTransactionID.Value);
-
-            var banktransfers = xeroAPI.BankTransfers();
-            if (banktransfers != null && banktransfers.Count > 0)
-            {
-                var singlebanktransfer = xeroAPI.BankTransaction(banktransfers[3].BankTransferID.Value);
-            }
-
-            var taxrates = xeroAPI.TaxRates();
-            if (taxrates != null && taxrates.Count > 0)
-            {
-                var singletaxrate = xeroAPI.TaxRate(taxrates[3].Name);
-            }
+            //var trackingcats = xeroAPI.TrackingCategories();
+            //if (trackingcats != null && trackingcats.Count > 0)
+            //{
+            //    var singlecat = xeroAPI.TrackingCategory(trackingcats[0].TrackingCategoryID.Value);
+            //}
 
 
-            var products = xeroAPI.Items();
-            var singleproduct = xeroAPI.Item(products[3].ItemID.Value);
+            //var banktrans = xeroAPI.BankTransactions();
+            //var singleptran = xeroAPI.BankTransaction(banktrans[3].BankTransactionID.Value);
+
+            //var banktransfers = xeroAPI.BankTransfers();
+            //if (banktransfers != null && banktransfers.Count > 0)
+            //{
+            //    var singlebanktransfer = xeroAPI.BankTransaction(banktransfers[3].BankTransferID.Value);
+            //}
+
+            //var taxrates = xeroAPI.TaxRates();
+            //if (taxrates != null && taxrates.Count > 0)
+            //{
+            //    var singletaxrate = xeroAPI.TaxRate(taxrates[3].Name);
+            //}
+
+
+            //var products = xeroAPI.Items();
+            //var singleproduct = xeroAPI.Item(products[3].ItemID.Value);
 
 
             var invoices = xeroAPI.Invoices();
             var singleinvoice = xeroAPI.Invoice(invoices[5].InvoiceID.Value);
 
-            var accounts = xeroAPI.Accounts(); // Return List<Xero.NetStandard.OAuth2.Model.Accounting.Account>
-            var singleaccount = xeroAPI.Account(accounts[5].AccountID.Value);// return Xero.NetStandard.OAuth2.Model.Accounting.Account
+            //var accounts = xeroAPI.Accounts(); // Return List<Xero.NetStandard.OAuth2.Model.Accounting.Account>
+            //var singleaccount = xeroAPI.Account(accounts[5].AccountID.Value);// return Xero.NetStandard.OAuth2.Model.Accounting.Account
 
-            var quotes = xeroAPI.Quotes();
-            if (quotes != null && quotes.Count > 0)
-            {
-                var singlequote = xeroAPI.Quote(quotes[0].QuoteID.Value);// return Xero.NetStandard.OAuth2.Model.Accounting.Quote
-            }
+            //var quotes = xeroAPI.Quotes();
+            //if (quotes != null && quotes.Count > 0)
+            //{
+            //    var singlequote = xeroAPI.Quote(quotes[0].QuoteID.Value);// return Xero.NetStandard.OAuth2.Model.Accounting.Quote
+            //}
 
 
             int h = 0;
@@ -155,15 +155,53 @@ namespace XeroAPI2Tests
             System.Diagnostics.Debug.WriteLine(e.MessageText);
 
             WriteLogFile($"{e.Status.ToString()} - {e.MessageText}", "APILog", true, true);
-
-
+            UpdateStatus($"{e.Status.ToString()} - {e.MessageText}", lstResults);
         }
+        public static void UpdateStatus(string sText, ListBox lstResults = null, bool bSameLine = false, bool bAdd = false)
+        {
+            if (lstResults.InvokeRequired)
+            {
+                lstResults.Invoke((MethodInvoker)delegate
+                {
+                    UpdateStatus(sText, lstResults);
+                });
+            }
+            else
+            {                
+                if (lstResults != null)
+                {
+                    // Passed in a ref to a list box so we can update this  s
+                    if (bSameLine && bAdd)
+                    {
+                        string PrevLine = lstResults.Items[lstResults.Items.Count - 1].ToString();
+                        lstResults.Items[lstResults.Items.Count - 1] = PrevLine + sText;
+                    }
+                    else if (bSameLine && !bAdd)
+                    {
+                        lstResults.Items[lstResults.Items.Count - 1] = sText;
+                    }
+                    else
+                    {
+                        // sText = String.Format("({0}) {1}", DateTime.Parse(DateTime.Now.ToString(), ciUKCulture.DateTimeFormat).ToString(), sText);
+                        lstResults.Items.Add(sText);
+                    }
+                    if (lstResults.Items.Count > 65000)
+                    {
+                        lstResults.Items.RemoveAt(1);
+                    }
+                    lstResults.TopIndex = lstResults.Items.Count - 10;
+                    lstResults.Refresh();
 
+                }
+
+            }
+        }
 
         public void WriteLogFile(string sText, string sLogFileName, bool bTimeStamp = false, bool appendNewLine = true)
         {
             try
             {
+
                 string sPath = Path.Combine(ApplicationPath, "Logs", $"{sLogFileName}.log");
 
                 //   string sPath = String.Format(@"{0}\logs\{1}.log", ApplicationPath, sLogFileName);
